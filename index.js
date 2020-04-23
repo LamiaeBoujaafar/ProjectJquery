a94fae057c189e457835cd731eb76c4e5974ebb4
lamiae boujaafar <lamiae20ber@gmail.com>
2020-04-23 18:02:00 +0000

    modifie

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 $(function(){
     var $mainmenuItems = $("#main-menu ul").children("li"),
     total = $mainmenuItems.length,
-        openIndex = -1,
+        openIndex = 2,
         init = function(){
-            $mainmenuItems.children(".image").click(function(){
-                var newIndex = $(this).parent().index(),
-                $items = $mainmenuItems.eq(newIndex),
-                $colorImage = $items.find(".color");
-                $colorImage.animate({left : "0px"},250);
-                $items.animate({width:"420px"},250);
-                openIndex = newIndex;
-                
-                
-            });
+            bindEvents();
+        if(validIndex(openIndex))
+            {
+                animate($mainmenuItems.eq(openIndex),true,700);
+            }
         };
+    bindEvents = function(){
+        $mainmenuItems.children(".image").click(function(){
+                var newIndex = $(this).parent().index(),
+                $items = $mainmenuItems.eq(newIndex);
+                if(openIndex === newIndex)
+                {
+                    animate($items,false,250);
+                    openIndex = -1;
+                }else
+                {
+                    if(validIndex(newIndex))
+                        {
+                            animate($mainmenuItems.eq(openIndex),false,250);
+                            openIndex = newIndex;
+                            animate($items,true,250);
+                        }
+                }   
+            });
+        
+    }
+    validIndex = function(indexToceck)
+    {
+        return (indexToceck >= 0) && (indexToceck <total );
+    }
+    animate = function($items,bool,speed){
+        var $colorImage = $items.find(".color"),
+        paraItems = bool ? {width:"420px"}:{width:"140px"},
+        ColorImageParm = bool ? {left : "0px"}:{left : "140px"}
+        $colorImage.animate(ColorImageParm,speed);
+        $items.animate(paraItems,speed);
+    }
+    
     init();
 });
